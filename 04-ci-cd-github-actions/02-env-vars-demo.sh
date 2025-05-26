b21d2196857c5a3b7272aee41af598cb4ca41c1c
Priyanka Verma <priyanka@lexres.co.in>
2025-05-26 09:31:07 +0530

    Add GitHub Actions demo for environment variables

---
+++ 04-ci-cd-github-actions/02-env-vars-demo.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+echo "Running Environment Variables Demo Script"
+
+echo "GitHub Actor: $GITHUB_ACTOR"
+echo "Repository Name: $GITHUB_REPOSITORY"
+echo "Event Name: $GITHUB_EVENT_NAME"
