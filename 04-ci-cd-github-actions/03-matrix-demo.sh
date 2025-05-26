50746c4d298df1d887c4fd8638a1ff4252dc26be
Priyanka Verma <priyanka@lexres.co.in>
2025-05-26 09:54:40 +0530

    Add GitHub Actions matrix demo workflow with shell script

---
+++ 04-ci-cd-github-actions/03-matrix-demo.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+echo "Running script on OS: $RUNNER_OS"
+echo "Using Node version: $NODE_VERSION"
+
+# Simulate a task, e.g., npm install and test (just echo here)
+echo "Simulating build and test for Node $NODE_VERSION on $RUNNER_OS"
